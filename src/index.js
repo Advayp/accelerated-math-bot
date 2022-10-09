c7662f6f4193dbb03067d69b04e460396f025c17
Advay Patil <advaypatil27@gmail.com>
2022-10-09 16:56:55 -0700

    setup discord.js in `index.js`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World");
+import { Client, GatewayIntentBits } from "discord.js";
+import config from "../config.json" assert { type: "json" };
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once("ready", () => {
+  console.log("Ready");
+});
+
+client.login(config["token"]);
